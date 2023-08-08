bc1fe2e1ed7d609101a8fdfeb8a96ec2bbdad194
ahmutysj3 <ahmutysj3@gmail.com>
2023-08-08 15:06:24 -0400

    added ami and az data source blocks for pfsense

--- aws/peered_w_vpn/data.tf
+++ aws/peered_w_vpn/data.tf
@@ -15,3 +15,34 @@ data "aws_vpc_peering_connection" "hub_db" {
   vpc_id      = aws_vpc.hub.id
   peer_vpc_id = aws_vpc.db.id
 }
+
+data "aws_ami" "pfsense" {
+  most_recent      = true
+  name_regex       = "pfSense-plus-ec2-23.05-RELEASE-amd64"
+  owners           = ["aws-marketplace"]
+
+  filter {
+    name   = "boot-mode"
+    values = ["uefi"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "default" {
+  state = "available"
+}
+
+locals {
+  az1 = data.aws_availability_zones.default.names[0]
+  az2 = data.aws_availability_zones.default.names[1]
+  pfsense_ami = data.aws_ami.pfsense.id
+}
