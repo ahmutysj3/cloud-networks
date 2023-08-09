04669dd0253a069127008c300a80cb55bb4e3fc0
ahmutysj3 <ahmutysj3@gmail.com>
2023-08-09 17:40:11 -0400

    network interface work

--- aws/peered_w_vpn/data.tf
+++ aws/peered_w_vpn/data.tf
@@ -2,18 +2,21 @@ data "aws_vpc_peering_connection" "hub_dmz" {
   depends_on  = [aws_vpc_peering_connection.hub_dmz]
   vpc_id      = aws_vpc.hub.id
   peer_vpc_id = aws_vpc.dmz.id
+  status = "active"
 }
 
 data "aws_vpc_peering_connection" "hub_app" {
   depends_on  = [aws_vpc_peering_connection.hub_app]
   vpc_id      = aws_vpc.hub.id
   peer_vpc_id = aws_vpc.app.id
+  status = "active"
 }
 
 data "aws_vpc_peering_connection" "hub_db" {
   depends_on  = [aws_vpc_peering_connection.hub_db]
   vpc_id      = aws_vpc.hub.id
   peer_vpc_id = aws_vpc.db.id
+  status = "active"
 }
 
 data "aws_ami" "pfsense" {
@@ -39,40 +42,4 @@ data "aws_ami" "pfsense" {
 
 data "aws_availability_zones" "default" {
   state = "available"
-}
-
-resource "aws_instance" "pfsense" {
-    depends_on = [ aws_subnet.hub_outside ]
-  ami = data.aws_ami.pfsense.id
-  availability_zone = data.aws_availability_zones.default.names[0]
-  instance_type = "t3.nano"
-  tags = {
-    Name = "pfsense"
-  }
-  key_name = aws_key_pair.pfsense.key_name
-  private_ip = cidrhost(aws_subnet.hub_outside.cidr_block,4)
-  subnet_id = aws_subnet.hub_outside.id
-
-  cpu_options {
-    core_count = 1
-    threads_per_core = 2
-  }
-  
-}
-
-resource "aws_network_interface" "pfsense_inside" {
-    depends_on = [ aws_subnet.hub_inside ]
-  subnet_id       = aws_subnet.hub_inside.id
-  private_ip     = cidrhost(aws_subnet.hub_inside.cidr_block,3)
-  tags = {
-    Name = "pfsense_inside_nic"
-  }
-}
-
-resource "aws_key_pair" "pfsense" {
-  key_name = "deploy"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    Name = "vmbox-ubuntu-key"
-  }
 }
---
+++ aws/peered_w_vpn/firewall.tf
@@ -0,0 +1,40 @@
+
+resource "aws_instance" "pfsense" {
+    depends_on = [ aws_subnet.hub_outside ]
+  ami = data.aws_ami.pfsense.id
+  availability_zone = data.aws_availability_zones.default.names[0]
+  instance_type = "t3.nano"
+  tags = {
+    Name = "pfsense"
+  }
+  key_name = aws_key_pair.pfsense.key_name
+
+  cpu_options {
+    core_count = 1
+    threads_per_core = 2
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.pfsense_outside.id
+    device_index = 0
+    network_card_index = 0
+  }
+  
+}
+
+resource "aws_network_interface" "pfsense_outside" {
+    depends_on = [ aws_subnet.hub_outside ]
+  subnet_id       = aws_subnet.hub_outside.id
+  private_ip     = cidrhost(aws_subnet.hub_outside.cidr_block,3)
+  tags = {
+    Name = "pfsense_outside_nic"
+  }
+}
+
+resource "aws_key_pair" "pfsense" {
+  key_name = "deploy"
+  public_key = file("~/.ssh/id_rsa.pub")
+  tags = {
+    Name = "vmbox-ubuntu-key"
+  }
+}
