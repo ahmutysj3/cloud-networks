6cd33535e05aba78071b4135b29eeb3f21d952fe
Trace Ahmuty <ahmutysj3@gmail.com>
2023-08-17 13:45:47 -0400

    added vault config

--- aws/peered_w_vpn/providers.tf
+++ aws/peered_w_vpn/providers.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region  = var.aws_region
   profile = "default"
 }
+
+provider "vault" {
+  address = "https://vault.tracecloud.us:8200"
+  auth_login_cert {
+    cert_file = "/home/trace/vault-client-certs/terraform_server.crt"
+    key_file = "/home/trace/vault-client-certs/terraform_server.pem"
+    mount = "cert"
+  }
+}
