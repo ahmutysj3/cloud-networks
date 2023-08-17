e19327092390115e464e584eeaf9cb486f3d4357
Trace Ahmuty <ahmutysj3@gmail.com>
2023-08-17 15:42:56 -0400

    added test secret resrouce

--- aws/peered_w_vpn/providers.tf
+++ aws/peered_w_vpn/providers.tf
@@ -14,7 +14,29 @@ provider "vault" {
 
 data "vault_auth_backends" "example" {}
 
-data "vault_kv_secret_v2" "example" {
-  mount = "kv"
-  name  = "terraform/aws"
+resource "vault_mount" "kvv2" {
+  path        = "test-kv"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
+
+resource "vault_kv_secret_v2" "example" {
+  mount                      = vault_mount.kvv2.path
+  name                       = "secret"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(
+  {
+    zip       = "zap",
+    foo       = "bar"
+  }
+  )
+  custom_metadata {
+    max_versions = 5
+    data = {
+      foo = "vault@example.com",
+      bar = "12345"
+    }
+  }
 }
