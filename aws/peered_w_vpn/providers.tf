d6706b0e58ccdaea4fdf0b1c103258142ac3e5b9
Trace Ahmuty <ahmutysj3@gmail.com>
2023-08-17 15:51:41 -0400

    updated mount name

--- aws/peered_w_vpn/providers.tf
+++ aws/peered_w_vpn/providers.tf
@@ -22,7 +22,7 @@ resource "vault_mount" "kvv2" {
 }
 
 resource "vault_kv_secret_v2" "example" {
-  mount                      = "kv"
+  mount                      = "test-kv"
   name                       = "secret"
   cas                        = 1
   delete_all_versions        = true
