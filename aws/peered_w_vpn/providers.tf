cf8472d3533f0541009069cd54c83a7cd15c3b76
Trace Ahmuty <ahmutysj3@gmail.com>
2023-08-17 15:56:54 -0400

    asdf

--- aws/peered_w_vpn/providers.tf
+++ aws/peered_w_vpn/providers.tf
@@ -12,17 +12,15 @@ provider "vault" {
   }
 }
 
-data "vault_auth_backends" "example" {}
-
 resource "vault_mount" "kvv2" {
-  path        = "test-kv"
+  path        = "kvv2"
   type        = "kv"
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
 
 resource "vault_kv_secret_v2" "example" {
-  mount                      = "test-kv"
+  mount                      = vault_mount.kvv2.path
   name                       = "secret"
   cas                        = 1
   delete_all_versions        = true
