2a5c6cbbc291e7989062ae7595d8f027c2ef6276
Trace Ahmuty <ahmutysj3@gmail.com>
2023-08-17 20:49:18 -0400

    added test datablock

--- aws/peered_w_vpn/providers.tf
+++ aws/peered_w_vpn/providers.tf
@@ -15,22 +15,6 @@ provider "vault" {
 variable "vault_client_cert" {}
 variable "vault_client_key" {}
   
-resource "vault_kv_secret_v2" "test" {
-  mount                      = "terraform"
-  name                       = "secret"
-  cas                        = 1
-  delete_all_versions        = true
-  data_json                  = jsonencode(
-  {
-    zip       = "zap",
-    foo       = "bar"
-  }
-  )
-  custom_metadata {
-    max_versions = 5
-    data = {
-      foo = "vault@example.com",
-      bar = "12345"
-    }
-  }
+data "vault_kv_secret" "aws" {
+  path = "terraform/aws"
 }
