525d461cd2a20b8a9ea0bb38d674aa7b4d0f461c
trace ahmuty <ahmutysj3@gmail.com>
2023-08-18 19:14:19 +0000

    test from vscode integrated in ssh term

---
+++ aws/peered_w_vpn/vault.tf
@@ -0,0 +1,17 @@
+provider "vault" {
+  auth_login_cert {
+    cert_file = var.vault_client_cert
+    key_file = var.vault_client_key
+    mount = "cert"
+  }
+}
+
+# set ENV variables for vault
+variable "vault_client_cert" {}
+variable "vault_client_key" {}
+  
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "terraform-user"
+}
+
