49a83f4698d2fe2c895be0acdb6d99f134414e5d
trace ahmuty <ahmutysj3@gmail.com>
2023-08-19 13:31:58 +0000

    added dynamodb backend

--- aws/peered_w_vpn/versions.tf
+++ aws/peered_w_vpn/versions.tf
@@ -1,10 +1,11 @@
 terraform {
-
   backend "s3" {
-    bucket = "trace-tf-unlocked-bucket"
-    key    = "aws/vpc/peered-w-vpn/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "trace-terraform-bucket"
+    key            = "aws/network/peered-w-vpn/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_db"
   }
+
 
   required_providers {
     vault = {
