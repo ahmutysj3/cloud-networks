8b8a8dca8031f07866218c93eedc3931635e9226
trace ahmuty <ahmutysj3@gmail.com>
2023-08-22 02:34:00 +0000

    added consul backend

--- aws/peered_w_vpn/versions.tf
+++ aws/peered_w_vpn/versions.tf
@@ -1,11 +1,18 @@
 terraform {
-  backend "s3" {
+  /* backend "s3" {
     bucket         = "trace-terraform-bucket"
     key            = "aws/network/peered-w-vpn/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_db"
-  }
+  } */
 
+  backend "consul" {
+    address = "consul-01.tracecloud.us:8500"
+    scheme  = "http"
+    lock = true 
+    gzip = false
+    path = "aws/peered-w-vpn/terraform.tfstate"
+  }
 
   required_providers {
     vault = {
