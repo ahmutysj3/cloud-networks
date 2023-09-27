32edf3881d9e78a5c886e1b4a4f8a632fc60cfd2
trace ahmuty <ahmutysj3@gmail.com>
2023-09-27 21:40:14 +0000

    terraform fmt

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,25 +1,25 @@
 resource "google_compute_network" "hub" {
-  project                 = var.gcp_project
+  project = var.gcp_project
   #auto_create_subnetworks = false
-  name                    = "hub-vpc"
+  name = "hub-vpc"
 }
 
 resource "google_compute_network" "spoke1" {
-  project                 = var.gcp_project
+  project = var.gcp_project
   #auto_create_subnetworks = false
-  name                    = "spoke1-vpc"
+  name = "spoke1-vpc"
 }
 
 resource "google_compute_network" "spoke2" {
-  project                 = var.gcp_project
+  project = var.gcp_project
   #auto_create_subnetworks = false
-  name                    = "spoke2-vpc"
+  name = "spoke2-vpc"
 }
 
 resource "google_compute_network" "spoke3" {
-  project                 = var.gcp_project
+  project = var.gcp_project
   #auto_create_subnetworks = false
-  name                    = "spoke3-vpc"
+  name = "spoke3-vpc"
 }
 
 resource "google_compute_subnetwork" "fw_inside" {
