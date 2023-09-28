ffbcb336211fd2e70b9edcecc635b23fbdfbfa44
trace ahmuty <ahmutysj3@gmail.com>
2023-09-28 18:23:52 +0000

    turned off subnet autocreate in vpcs

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,24 +1,24 @@
 resource "google_compute_network" "hub" {
   project = var.gcp_project
-  #auto_create_subnetworks = false
+  auto_create_subnetworks = false
   name = "hub-vpc"
 }
 
 resource "google_compute_network" "spoke1" {
   project = var.gcp_project
-  #auto_create_subnetworks = false
+  auto_create_subnetworks = false
   name = "spoke1-vpc"
 }
 
 resource "google_compute_network" "spoke2" {
   project = var.gcp_project
-  #auto_create_subnetworks = false
+  auto_create_subnetworks = false
   name = "spoke2-vpc"
 }
 
 resource "google_compute_network" "spoke3" {
   project = var.gcp_project
-  #auto_create_subnetworks = false
+  auto_create_subnetworks = false
   name = "spoke3-vpc"
 }
 
