cec6a798f5a2e24792d412c792b8d194a0b936f2
trace ahmuty <ahmutysj3@gmail.com>
2023-09-30 14:11:13 +0000

    removed extra firewall rule

--- gcp/firewalls.tf
+++ gcp/firewalls.tf
@@ -1,19 +1,3 @@
-/* resource "google_compute_firewall" "hub_trusted" {
-  name    = "test-firewall"
-  network = google_compute_network.hub.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "443"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-} */
-
 resource "google_compute_firewall" "allow_all" {
   name    = "test-firewall"
   network = google_compute_network.hub.name
