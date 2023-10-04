60b6b5cabcd43113c8882654a92dd9d04499fee4
trace ahmuty <ahmutysj3@gmail.com>
2023-10-04 20:48:31 +0000

    deployed forti instance

--- gcp/instance.tf
+++ gcp/instance.tf
@@ -1,10 +1,10 @@
-/* resource "google_compute_instance" "fortigate-active" {
+resource "google_compute_instance" "fortigate-active" {
   name           = "fgt-active-fw"
   machine_type   = "e2-standard-4"
   zone           = data.google_compute_zones.available.names[0]
   can_ip_forward = "true"
-  tags    = ["allow-fgt-mgmt", "allow-fgt-trusted", "allow-fgt-untrusted", "allow-fgt-ha"]
-  project = var.gcp_project
+  tags           = ["allow-fgt-mgmt", "allow-fgt-trusted", "allow-fgt-untrusted", "allow-fgt-ha"]
+  project        = var.gcp_project
   metadata = {
     fortigate_user_password  = "yhhR.8u7"
     google-logging-enable    = "0"
@@ -22,66 +22,67 @@
   boot_disk {
     auto_delete = true
     device_name = "boot-disk"
-
-    initialize_params {
-      image = data.google_compute_image.fortigate.self_link
-    }
-
     mode   = "READ_WRITE"
     source = google_compute_disk.boot.self_link
   }
 
-  network_interface {
+  network_interface { # GUI/MGMT Interface
     access_config {
-      nat_ip       = "35.229.111.55"
+      nat_ip       = google_compute_address.fw_mgmt_external.address
       network_tier = "PREMIUM"
     }
 
-    network            = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/global/networks/mgmt-network"
-    network_ip         = "10.99.254.132"
+    network            = google_compute_network.mgmt.self_link
+    network_ip         = google_compute_address.fw_mgmt.address
     stack_type         = "IPV4_ONLY"
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/regions/us-east1/subnetworks/admin-subnet"
-    subnetwork_project = "terraform-project-trace-lab"
+    subnetwork         = google_compute_subnetwork.fw_mgmt.self_link
+    subnetwork_project = var.gcp_project
   }
 
-  network_interface {
+  network_interface { # WAN Interface
     access_config {
-      nat_ip       = "35.196.230.250"
+      nat_ip       = google_compute_address.fw_wan_external.address
       network_tier = "PREMIUM"
     }
 
-    network            = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/global/networks/untrusted-network"
-    network_ip         = "10.99.1.3"
+    network            = google_compute_network.untrusted.self_link
+    network_ip         = google_compute_address.fw_wan.address
     stack_type         = "IPV4_ONLY"
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/regions/us-east1/subnetworks/fw-outside-subnet"
-    subnetwork_project = "terraform-project-trace-lab"
+    subnetwork         = google_compute_subnetwork.fw_untrusted.self_link
+    subnetwork_project = var.gcp_project
   }
 
-  network_interface {
-    network            = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/global/networks/fw-ha-network"
-    network_ip         = "10.99.255.4"
+  network_interface { # HA SYNC Interface
+    network            = google_compute_network.ha_sync.self_link
+    network_ip         = google_compute_address.fw_ha_sync.address
     stack_type         = "IPV4_ONLY"
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/regions/us-east1/subnetworks/fw-ha-subnet"
-    subnetwork_project = "terraform-project-trace-lab"
+    subnetwork         = google_compute_subnetwork.fw_ha_sync.self_link
+    subnetwork_project = var.gcp_project
   }
 
-  network_interface {
-    network            = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/global/networks/trusted-network"
-    network_ip         = "10.99.0.3"
+  network_interface { # LAN Interface
+    network            = google_compute_network.trusted.self_link
+    network_ip         = google_compute_address.fw_lan.address
     stack_type         = "IPV4_ONLY"
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/terraform-project-trace-lab/regions/us-east1/subnetworks/fw-inside-subnet"
-    subnetwork_project = "terraform-project-trace-lab"
+    subnetwork         = google_compute_subnetwork.fw_trusted.self_link
+    subnetwork_project = var.gcp_project
   }
 
-  scheduling {
+  /* scheduling { # Normal Rates
     automatic_restart   = true
     on_host_maintenance = "MIGRATE"
     provisioning_model  = "STANDARD"
+  } */
+
+  scheduling { # Discounted Rates
+    automatic_restart  = false
+    preemptible        = true
+    provisioning_model = "SPOT"
   }
 
   service_account {
-    email  = "215092868248-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud.useraccounts.readonly", "https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write"]
+    email  = data.google_compute_default_service_account.default.email
+    scopes = ["cloud-platform"]
   }
 
   shielded_instance_config {
@@ -89,7 +90,7 @@
     enable_vtpm                 = true
   }
 
-} */
+}
 
 resource "google_compute_disk" "boot" {
   image                     = data.google_compute_image.fortigate.self_link
@@ -112,8 +113,8 @@ resource "google_compute_disk" "log" {
 
 data "google_compute_image" "fortigate" {
   provider = google
-  family  = "fortigate-74-payg"
-  project = "fortigcp-project-001"
+  family   = "fortigate-74-payg"
+  project  = "fortigcp-project-001"
 }
 
 data "google_compute_zones" "available" {
@@ -141,7 +142,7 @@ resource "google_compute_address" "fw_mgmt" {
   address      = cidrhost(google_compute_subnetwork.fw_mgmt.ip_cidr_range, 2)
 }
 
-resource "google_compute_address" "fw_inside" {
+resource "google_compute_address" "fw_lan" {
   name         = "fw-inside-ip"
   address_type = "INTERNAL"
   purpose      = "GCE_ENDPOINT"
@@ -150,7 +151,7 @@ resource "google_compute_address" "fw_inside" {
   address      = cidrhost(google_compute_subnetwork.fw_trusted.ip_cidr_range, 2)
 }
 
-resource "google_compute_address" "fw_outside" {
+resource "google_compute_address" "fw_wan" {
   name         = "fw-outside-ip"
   address_type = "INTERNAL"
   purpose      = "GCE_ENDPOINT"
@@ -160,15 +161,15 @@ resource "google_compute_address" "fw_outside" {
 }
 
 resource "google_compute_address" "fw_wan_external" {
-  name = "fw-wan-external-ip"
+  name         = "fw-wan-external-ip"
   address_type = "EXTERNAL"
-  ip_version = "IPV4"
-  region = var.gcp_region
+  ip_version   = "IPV4"
+  region       = var.gcp_region
 }
 
 resource "google_compute_address" "fw_mgmt_external" {
-  name = "fw-mgmt-external-ip"
+  name         = "fw-mgmt-external-ip"
   address_type = "EXTERNAL"
-  ip_version = "IPV4"
-  region = var.gcp_region
+  ip_version   = "IPV4"
+  region       = var.gcp_region
 }
