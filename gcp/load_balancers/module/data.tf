e3b5d6fd20b826cbc9a18d4095a96c13b6216127
Trace Ahmuty <ahmutysj3@gmail.com>
2023-10-26 22:48:57 -0400

    good test with just igs

--- gcp/load_balancers/module/data.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  instance_groups_tuple = [for instance_group in var.instance_groups : instance_group]
-  instance_groups       = { for instance_group in local.instance_groups_tuple : instance_group.instance_grp => instance_group }
-}
-
-data "google_compute_network" "this" {
-  project = var.project
-  name    = var.network
-}
--- gcp/load_balancers/module/main.tf
+++ gcp/load_balancers/module/main.tf
@@ -1,3 +1,13 @@
+locals {
+  instance_groups_tuple = [for instance_group in var.instance_groups : instance_group]
+  instance_groups       = { for instance_group in local.instance_groups_tuple : instance_group.instance_grp => instance_group }
+}
+
+data "google_compute_network" "this" {
+  project = var.project
+  name    = var.network
+}
+
 module "instance_groups" {
   source    = "./instance_groups"
   for_each  = local.instance_groups
@@ -7,6 +17,3 @@ module "instance_groups" {
   network   = data.google_compute_network.this.self_link
   instances = each.value.instances
 }
-
-
-
