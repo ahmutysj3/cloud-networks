ee7c7568b7d355d5bb1afe4f17dc81a86436e878
Trace Ahmuty <ahmutysj3@gmail.com>
2023-11-05 00:10:54 -0400

    added internal loadbalancer

--- gcp/networks/fortigate-lb-sandwich/main.tf
+++ gcp/networks/fortigate-lb-sandwich/main.tf
@@ -59,6 +59,14 @@ resource "google_compute_subnetwork" "untrusted" {
   purpose       = "PRIVATE"
   stack_type    = "IPV4_ONLY"
 }
+resource "google_compute_route" "untrusted_inet" {
+  project          = var.gcp_project
+  network          = google_compute_network.untrusted.name
+  name             = "untrusted-inet-route"
+  dest_range       = "0.0.0.0/0"
+  priority         = 1000
+  next_hop_gateway = "default-internet-gateway"
+}
 
 resource "google_compute_subnetwork" "protected" {
   project       = var.gcp_project
@@ -135,10 +143,10 @@ resource "google_compute_instance" "pfsense" {
   name           = "pfsense-active-fw"
   machine_type   = "e2-standard-4"
   zone           = data.google_compute_zones.available.names[0]
-  can_ip_forward = "true"
+  can_ip_forward = true
   project        = var.gcp_project
   metadata = {
-    enable-oslogin           = "true"
+    enable-oslogin           = true
     google-logging-enable    = "0"
     google-monitoring-enable = "0"
     ssh-keys                 = "trace:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjI2kHRd2kAMmb8wbVmu66q/MfHhGiop6tZ1s7e9iJ+TzOK0S92cfIxrBTu08J6MhTg/CUfZwHe6WKB3sA5A2tWOLLpYdkvvwAojOh0z7hD9l8UZ57agRu0aaVfOofQwhQBWZFiOWIOUWmLAtHCxejV24ICJt/+pk1D+0MhqulKccC1Si7RZgzBqGzeH64mwgTbbl/QD3Hf2NcT5PvUZL9yWJDonoh1CZ5j4SfU/YJBBQXXsI3LJkH5gGCz2+CY+ZhZbtnCLrDMsgzK9uUSamdZ7bIiBi0LAM8P9O+QK75kBwnyRvQly92sIP50uxMGAfI8D/MfmHoP9pcTmHFbWcv trace@trace-laptop"
@@ -153,11 +161,6 @@ resource "google_compute_instance" "pfsense" {
 
 
   network_interface { # nic0: WAN Interface
-    access_config {
-      nat_ip       = google_compute_address.wan_external.address
-      network_tier = "PREMIUM"
-    }
-
     nic_type           = "VIRTIO_NET"
     network            = google_compute_network.untrusted.name
     network_ip         = google_compute_address.wan_private.address
@@ -200,7 +203,7 @@ resource "google_compute_disk" "pfsense_boot" {
 
 data "google_compute_image" "pfsense" {
   project     = var.gcp_project
-  most_recent = "true"
+  most_recent = true
   name        = "pfsense-gcp-image"
 }
 
@@ -227,4 +230,72 @@ resource "google_compute_address" "wan_external" {
   address_type = "EXTERNAL"
   ip_version   = "IPV4"
   region       = var.gcp_region
+}
+
+
+resource "google_compute_region_health_check" "pfsense" {
+  name               = "pfsense-tcp-health-check"
+  description        = "Health check via tcp"
+  project            = var.gcp_project
+  region             = var.gcp_region
+  timeout_sec        = 2
+  check_interval_sec = 2
+  tcp_health_check {
+    port = 443
+  }
+}
+
+resource "google_compute_region_backend_service" "pfsense_wan" {
+  # provider              = google-beta
+  region                = var.gcp_region
+  project               = var.gcp_project
+  name                  = "pfsense-backend-service"
+  health_checks         = [google_compute_region_health_check.pfsense.id]
+  protocol              = "UNSPECIFIED"
+  load_balancing_scheme = "EXTERNAL"
+  backend {
+    group = google_compute_instance_group.pfsense_wan.self_link
+  }
+
+}
+
+resource "google_compute_instance_group" "pfsense_wan" {
+  project   = var.gcp_project
+  zone      = google_compute_instance.pfsense.zone
+  name      = "pfsense-instance-group"
+  instances = [google_compute_instance.pfsense.self_link]
+  network   = google_compute_instance.pfsense.network_interface[0].network
+}
+
+resource "google_compute_instance_group" "pfsense_lan" {
+  project   = var.gcp_project
+  zone      = google_compute_instance.pfsense.zone
+  name      = "internal-instance-group"
+  instances = [google_compute_instance.pfsense.self_link]
+  network   = google_compute_instance.pfsense.network_interface[1].network
+}
+
+
+resource "google_compute_forwarding_rule" "pfsense_lan" {
+  name                  = "pfsense-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_address.wan_external.address
+  ip_protocol           = "L3_DEFAULT"
+  backend_service       = google_compute_region_backend_service.pfsense_lan.self_link
+  all_ports             = true
+  region                = var.gcp_region
+}
+
+resource "google_compute_region_backend_service" "pfsense_lan" {
+  # provider              = google-beta
+  region                = var.gcp_region
+  project               = var.gcp_project
+  name                  = "pfsense-lan-backend-service"
+  health_checks         = [google_compute_region_health_check.pfsense.id]
+  protocol              = "UNSPECIFIED"
+  load_balancing_scheme = "INTERNAL"
+  backend {
+    group = google_compute_instance_group.pfsense_lan.self_link
+  }
+
 }
