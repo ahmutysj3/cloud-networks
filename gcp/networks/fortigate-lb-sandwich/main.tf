ec9bcc76ced9fdee2669b6a586138e38f0197673
Trace Ahmuty <ahmutysj3@gmail.com>
2023-11-05 20:36:48 -0500

    increased boot disk size

--- gcp/networks/fortigate-lb-sandwich/main.tf
+++ gcp/networks/fortigate-lb-sandwich/main.tf
@@ -4,9 +4,3 @@ data "google_compute_zones" "available" {
 
 data "google_compute_default_service_account" "default" {
 }
-
-
-resource "google_compute_project_metadata_item" "default" {
-  key   = "serial-port-enable"
-  value = "true"
-}
