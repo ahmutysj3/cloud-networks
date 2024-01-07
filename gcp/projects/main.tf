3d95e6b0a2385e4be093c6e39e43cef948aa806c
Trace Ahmuty <ahmutysj3@gmail.com>
2024-01-07 18:07:55 -0500

    added shard vpc module

--- gcp/projects/main.tf
+++ gcp/projects/main.tf
@@ -6,26 +6,33 @@ locals {
   project_ids      = [for project in data.google_projects.this.projects : project.project_id]
   app_vpc_projects = [for project in local.project_ids : project if length(regexall("^trace-vpc-app-.*", project)) > 0]
   edge_project     = [for project in local.project_ids : project if length(regexall(var.edge_project, project)) > 0]
-  vm_project       = [for project in local.project_ids : project if length(regexall("^trace-vm-instance*", project)) > 0]
+  vm_projects      = [for project in local.project_ids : project if length(regexall("^trace-vm-instance*", project)) > 0]
 }
 
 module "edge_vpc_project" {
   for_each = toset(local.edge_project)
-  source   = "./services"
+  source   = "./modules/edge-network"
   project  = each.key
   services = var.edge_network_services
 }
 
 module "app_vpc_projects" {
-  source   = "./services"
+  source   = "./modules/app-networks"
   for_each = toset(local.app_vpc_projects)
   project  = each.key
   services = var.app_network_services
 }
 
 module "vm_project" {
-  for_each = toset(local.vm_project)
-  source   = "./services"
+  for_each = toset(local.vm_projects)
+  source   = "./modules/vm-projects"
   project  = each.key
   services = var.vm_services
+}
+
+module "shared_vpc" {
+  source           = "./modules/shared_vpc"
+  for_each         = toset(local.app_vpc_projects)
+  host_project     = each.key
+  service_projects = local.vm_projects
 }
---
+++ gcp/projects/modules/shared_vpc/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_shared_vpc_host_project" "this" {
+  provider = google-beta
+  project  = var.host_project
+}
+
+resource "google_compute_shared_vpc_service_project" "this" {
+  provider        = google-beta
+  for_each        = toset(var.service_projects)
+  service_project = each.key
+  host_project    = google_compute_shared_vpc_host_project.this.project
+}
+
+variable "host_project" {
+  type = string
+}
+
+variable "service_projects" {
+  type = list(string)
+}
