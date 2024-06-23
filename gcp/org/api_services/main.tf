e360dcc927ceddc0db3615ebe519c1d56ac2cc87
ahmutysj3 <ahmutysj3@gmail.com>
2024-06-23 00:38:04 -0400

    update api services gcp module

--- gcp/org/api_services/main.tf
+++ gcp/org/api_services/main.tf
@@ -1,73 +1,20 @@
+
+/* resource "google_project_service" "this" {
+  for_each = {}
+  project  = each.value
+  service  = each.key
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+ */
+
+
 locals {
-  vm_project_services   = { for k, v in toset(var.vm_services) : v => var.project_ids[var.project_names["vm"]] }
-  edge_project_services = { for k, v in toset(var.edge_network_services) : v => var.project_ids[var.project_names["edge"]] }
-  app_project_services  = { for k, v in toset(var.app_network_services) : v => var.project_ids[var.project_names["app_vpc"]] }
-  gke_project_services  = { for k, v in toset(var.gke_project_services) : v => var.project_ids[var.project_names["gke"]] }
+  definition_file  = file("${path.module}/api_services.yml")
+  project_services = { for k, v in yamldecode(local.definition_file).enable_apis : v.project => v.services }
 }
-
-resource "google_compute_shared_vpc_host_project" "this" {
-  provider = google-beta
-  project  = var.host_project
-}
-
-resource "google_compute_shared_vpc_service_project" "this" {
-  provider        = google-beta
-  for_each        = toset(var.service_projects)
-  service_project = each.key
-  host_project    = google_compute_shared_vpc_host_project.this.project
-}
-
-resource "google_project_service" "vm" {
-  for_each = local.vm_project_services
-  project  = each.value
-  service  = each.key
-
-  timeouts {
-    create = var.timeouts["create"]
-    update = var.timeouts["update"]
-  }
-
-  disable_dependent_services = var.disable_dependent_services
-}
-
-resource "google_project_service" "edge" {
-  for_each = local.edge_project_services
-  project  = each.value
-  service  = each.key
-
-  timeouts {
-    create = var.timeouts["create"]
-    update = var.timeouts["update"]
-  }
-
-  disable_dependent_services = var.disable_dependent_services
-}
-
-resource "google_project_service" "app" {
-  for_each = local.app_project_services
-  project  = each.value
-  service  = each.key
-
-  timeouts {
-    create = var.timeouts["create"]
-    update = var.timeouts["update"]
-  }
-
-  disable_dependent_services = var.disable_dependent_services
-}
-
-resource "google_project_service" "gke" {
-  for_each = local.gke_project_services
-  project  = each.value
-  service  = each.key
-
-  timeouts {
-    create = var.timeouts["create"]
-    update = var.timeouts["update"]
-  }
-
-  disable_dependent_services = var.disable_dependent_services
-}
-
-
-
